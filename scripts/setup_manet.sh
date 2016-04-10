093a7890eee3ba89ad300e8595bfea03a2ab2819
odroid C1 <odroid C1>
2016-04-11 08:17:31 +0930

    helpful scripts

---
+++ scripts/setup_manet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# parameters #
+NODEID=3
+TXPOWER=20
+CHANNEL=1
+WLAN=wlan0
+
+IPPREFIX=10.42.43
+NETMASK=24
+
+IPADDR=${IPPREFIX}.${NODEID}
+WLANM=`cat /sys/class/net/${WLAN}/address`
+#echo ${WLANM}
+
+
+sudo ./setup_wlan.sh ${WLAN} ${IPADDR}/${NETMASK} ${CHANNEL} ${TXPOWER} 
+
+sudo ip route del to ${IPPREFIX}.0/${NETMASK}
+
+
+sudo ../../click/userlevel/RNP_PKGclick WLANMAC=${WLANM} WLANINTERFACE=${WLAN} ADHOCIP=${IPADDR}/${NETMASK} ../../click/scripts/rnp_scripts/rnp_linux_args.click
